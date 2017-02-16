a912a11b18131fbac60b18a95753ceb75a666240
ssdavidson <ssdavidson@ucdavis.edu>
2017-02-16 11:03:32 -0800

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,15 @@
+import pickle
+from matplotlib import pyplot
+import nltk
+
+student_train_list = pickle.load(open("student_train.set", "rb"))
+
+results_list = list()
+
+for item in student_train_list:
+    essay = len(nltk.word_tokenize(item.essay))
+    tup = (essay, item.placement_raw)
+    results_list.append(tup)
+
+pyplot.plot(*zip(*results_list), 'g^')
+pyplot.show()
