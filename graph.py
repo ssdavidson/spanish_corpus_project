55638a086a7d0bb7faeaad727a85a62f9c804e29
ssdavidson <ssdavidson@ucdavis.edu>
2017-02-16 11:18:27 -0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,3 +1,6 @@
+#This is an example of how you can use the data to graph using pylot
+#in this case, graphing tokens used against placement score
+
 import pickle
 from matplotlib import pyplot
 import nltk
@@ -7,8 +10,8 @@ student_train_list = pickle.load(open("student_train.set", "rb"))
 results_list = list()
 
 for item in student_train_list:
-    essay = len(set(nltk.word_tokenize(item.essay)))
-    tup = (essay, item.placement_raw)
+    essay_tokens = len(set(nltk.word_tokenize(item.essay)))
+    tup = (essay_tokens, item.placement_raw)
     results_list.append(tup)
 
 pyplot.plot(*zip(*results_list), 'g^')
