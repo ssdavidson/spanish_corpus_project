073e6cf887cc869a05c816f9b9fedd4e52055a6d
ssdavidson <ssdavidson@ucdavis.edu>
2017-03-01 21:08:57 -0800

    Simplify code
    Update variable names and graph method to clarify and simplify code

--- graph.py
+++ graph.py
@@ -1,18 +1,16 @@
-#This is an example of how you can use the data to graph using pylot
-#in this case, graphing tokens used against placement score
-
 import pickle
 from matplotlib import pyplot
 import nltk
 
 student_train_list = pickle.load(open("student_train.set", "rb"))
 
-results_list = list()
+x = list()
+y = list()
 
 for item in student_train_list:
-    essay_tokens = len(set(nltk.word_tokenize(item.essay)))
-    tup = (essay_tokens, item.placement_raw)
-    results_list.append(tup)
+    types = len(set(nltk.word_tokenize(item.essay)))
+    x.append(types)
+    y.append(item.placement_raw)
 
-pyplot.plot(*zip(*results_list), 'g^')
+pyplot.plot(x, y, 'g^')
 pyplot.show()
